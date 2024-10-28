1f9b9f14368a9dc806b83d46295318fd667a47c3
Teodora Plopeanu <152170132+teodoraplopeanu@users.noreply.github.com>
2024-10-28 19:31:09 +0200

    Update identify_outliers.m

--- identify_outliers.m
+++ identify_outliers.m
@@ -1,8 +1,6 @@
-
 function outliers = identify_outliers(X, yval)
     [mean_values variances] = estimate_gaussian(X);
     probabilities = multivariate_gaussian(X, mean_values, variances);
     [epsilon F1 precision recall] = optimal_threshold(yval, probabilities);
     outliers = find(probabilities < epsilon);
-      %outliers = find(yval == 1);
 endfunction
